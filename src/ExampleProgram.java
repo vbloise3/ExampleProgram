c14dcd8dbd504baae091fac6096173f5fc94511f
Vince Bloise <vbloise3@verizon.net>
2016-04-24 20:11:55 -0400

    Added another stuff examples

--- src/ExampleProgram.java
+++ src/ExampleProgram.java
@@ -8,9 +8,11 @@ public class ExampleProgram {
     public static void main(String args[]) {
         System.out.printf("Example program, input parameter #1: %s\n", args[0]);
         SomeStuff theStuff = new SomeStuff();
+        OtherStuff theOtherStuff = new OtherStuff("Bloise", "@ Amazon!");
         theStuff.setName("The Dude");
         theStuff.setValue(23);
         System.out.printf("The stuff #1: %s - %d\n", theStuff.getName(), theStuff.getValue());
+        System.out.printf("The other stuff: %s %s\n", theOtherStuff.getTag(), theOtherStuff.getValue());
     }
 }
 
--- src/stuff/OtherStuff.java
+++ src/stuff/OtherStuff.java
@@ -7,6 +7,15 @@ public class OtherStuff {
     private String tag;
     private String value;
 
+    public OtherStuff(String tag, String value) {
+        this.setTag(tag);
+        this.setValue(value);
+    }
+
+    public OtherStuff() {
+
+    }
+
     public String getValue() {
         return value;
     }
