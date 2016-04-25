fd11853a15b03723edffdf2c57276abe7e8b5a37
Vince Bloise <vbloise3@verizon.net>
2016-04-24 20:06:47 -0400

    Added another stuff class

---
+++ src/stuff/OtherStuff.java
@@ -0,0 +1,26 @@
+package stuff;
+
+/**
+ * Created by vincebloise on 4/24/16.
+ */
+public class OtherStuff {
+    private String tag;
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getTag() {
+
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
--- src/stuff/SomeStuff.java
+++ src/stuff/SomeStuff.java
@@ -23,3 +23,4 @@ public class SomeStuff {
         this.value = value;
     }
 }
+
